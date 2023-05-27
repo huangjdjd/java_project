a03edaecfb878e29a6fd8c810f26c087170f49b9
frostbean <ylai88193@gmail.com>
2023-05-27 12:38:06 +0800

    Test money. Unsolvable problem.

--- src/java_project/java_project.java
+++ src/java_project/java_project.java
@@ -9,23 +9,26 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Arc;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class java_project extends Application {
-	@FXML
-	private Button return_main_d;
-	@FXML
-	private Button return_main_a;
-	@FXML 
-	private Button about_us;
-	@FXML
-	private Button start;
-	@FXML
-	private Button saying;
+	//Variables
+	public static int score = 0;
+	
+	//Components
+	@FXML private Button return_main_d;
+	@FXML private Button return_main_a;
+	@FXML private Button about_us;
+	@FXML private Button start;
+	@FXML private Button saying;
+	@FXML private Text score_number;
+	@FXML private Button buy_button_1;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 
-//		//main
+		//main
 		FXMLLoader main = new FXMLLoader(getClass().getResource("main.fxml"));
 		main.setController(this);
 		Parent root = main.load();
@@ -41,6 +44,11 @@ public class java_project extends Application {
 		about.setController(this);
 		Parent root_a = about.load();
 		Scene ascene=new Scene(root_a);
+		//測試遊戲頁面
+		FXMLLoader test = new FXMLLoader(getClass().getResource("test_game_background.fxml"));
+		about.setController(this);
+		Parent root_game = test.load();
+		Scene game_scene=new Scene(root_game);
 		
 		///onclick_main_說明
 		about_us.setOnAction(event->{
@@ -52,20 +60,37 @@ public class java_project extends Application {
 		});	
 		
 		//onclick_main_介紹我們
-		saying.setOnAction(event->{;
+		saying.setOnAction(event->{
 			primaryStage.setScene(ascene);
 		});	
 		//介紹返回主畫面
-		return_main_a.setOnAction(event->{;
+		return_main_a.setOnAction(event->{
 			primaryStage.setScene(mscene);
-		});	
+		});
+		
+		
+		//進入遊戲
+		start.setOnAction(event->{
+			score_up();
+			System.out.println(score);
+		    primaryStage.setScene(game_scene);
+		});
+		
+		//問題應該在這裡
+//		buy_button_1.setOnAction(event->{
+//			primaryStage.setScene(game_scene);
+//		});
 		
 		///設初始畫面
-		primaryStage.setTitle("Hello World");
+		primaryStage.setTitle("遊戲名稱");
 		primaryStage.setScene(mscene);
 		primaryStage.show();
 
-}
+    }
+	
+	public void score_up() {
+		score += 1;
+	}
 
 	public static void main(String[] args) {
 		launch(args);
